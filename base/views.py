a231bead0ddd1b91df795e79cdb1e4750d3b2fd7
Reis567 <matheusdosreis9@gmail.com>
2023-07-11 09:10:42 -0300

    Task detail view

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import *
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
